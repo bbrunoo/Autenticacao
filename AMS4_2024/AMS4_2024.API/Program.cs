fdcfb73c6c4e61c7bcf3e4b42b8ce38074b93119
bruno.sanntos <85880400+bbrunoo@users.noreply.github.com>
2024-05-21 22:22:50 -0300

    jwt

--- AMS4_2024/AMS4_2024.API/Program.cs
+++ AMS4_2024/AMS4_2024.API/Program.cs
@@ -9,7 +9,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 
 
-var key = Encoding.ASCII.GetBytes("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c");
+var key = Encoding.ASCII.GetBytes("secretkeyJWT");
 
 
 builder.Services.AddAuthentication(options =>
